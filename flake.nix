ceafd624e884ad7499f691bd2e3cb15a7714d629
tpyd <112725276+tpyd@users.noreply.github.com>
2026-04-25 09:41:09 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+    description = "Flakes file. Sort of like third party packages for NixOS configuration";
+
+    inputs = {
+        # Requirement for everything
+        nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+        # Allows configuration of applications and their config files using NixOS configuration
+        home-manager.url = "github:nix-community/home-manager";
+        # Can be combined into one scope with
+        # Apparently important to make home-managers nixpkgs dependency to follow the systems nixpkgs version
+        home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    outputs = { self, nixpkgs, home-manager, ... }: {
+        # The 'desktop' hostname is just for building, this is not the network hostname
+        nixosConfigurations.desktop = nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+
+            modules = [
+                # My configuration
+                ./configuration.nix
+
+                # Use home-manager or something when building
+                home-manager.nixosModules.home-manager
+
+                # Here is the actual home-manager configuration
+                {
+                    home-manager.users.hades = { pkgs, ... }: {
+                        home.stateVersion = "26.05";
+                        home.packages = with pkgs; [ 
+                            alacritty
+                            fish
+                            git
+                            tmux
+                            nerd-fonts.ubuntu-mono
+
+                            # Neovim
+                            neovim
+                            gcc  # Required for compiling treesitter parsers
+                            fzf  # Required by file picker fzf-lua
+                            tree-sitter  # Needed for Neovim tree-sitter support after rewrite
+                        ];
+
+                        xdg.configFile = {
+                            "alacritty/alacritty.toml".source = ./dotfiles/alacritty/alacritty.toml;
+                            "fish/config.fish".source = ./dotfiles/fish/config.fish;
+                            "git/config".source = ./dotfiles/git/config;
+                            "tmux/tmux.conf".source = ./dotfiles/tmux/tmux.conf;
+                        };
+                    };
+                }
+            ];
+        };
+    };
+}
